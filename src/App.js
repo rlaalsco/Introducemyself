83961b40ba54a650435a127862adccaec639781b
kimminchae <kmc050310@naver.com>
2024-07-22 23:41:36 +0900

    본인소개페이지

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <div>
+        본인소개페이지
+      </div>
+      <div>
+        이름:
+      </div>
+      <div>
+        <input type='text' value={"김민채"} maxLength='10' name='inform_name'/>
+      </div>
+    
+    </div>
+
+
+  );
+}
+
+function App() {
+  const topics = []
+}
+
+export default App;
+
